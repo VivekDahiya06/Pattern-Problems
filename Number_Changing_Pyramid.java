a9b5e2471fe969c571fe87c50d7a9f8ee9bcea97
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-07 10:05:05 +0530

    Added new file

---
+++ Number_Changing_Pyramid.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.Scanner;
+public class Number_Changing_Pyramid {
+    public static void main(String[] args) {
+        Scanner r= new Scanner(System.in);
+        System.out.println("Enter the no. of times to run the loop : ");
+        int n = r.nextInt();
+        int x=1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print(x+" ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
