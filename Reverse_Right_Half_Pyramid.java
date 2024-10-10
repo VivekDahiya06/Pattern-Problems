912527f514dd0bbed1fe2b8b26690af39ee6b863
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-10 22:33:35 +0530

    Add files via upload

---
+++ Reverse_Right_Half_Pyramid.java
@@ -0,0 +1,15 @@
+package Patterns;
+import java.util.Scanner;
+public class Reverse_Right_Half_Pyramid {
+    public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
+        System.out.println("Enter the no. of times to run the loop : ");
+        int n = r.nextInt();
+        for(int i=n; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
