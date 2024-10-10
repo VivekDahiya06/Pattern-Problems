01b8722300774e7c602118deebfa83bb5e59ea0c
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-10 22:33:06 +0530

    Add files via upload

---
+++ Palindrome_Triangle.java
@@ -0,0 +1,20 @@
+package Patterns;
+import java.util.Scanner;
+public class Palindrome_Triangle {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter the no. of times to run the loop : ");
+        int n = r.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i + 1; j > 0; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }}
