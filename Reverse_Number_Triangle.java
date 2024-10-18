beb2594b9f94e6d29838abb7bb24d472f2d96a90
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-18 23:03:32 +0530

    Add files via upload

---
+++ Reverse_Number_Triangle.java
@@ -0,0 +1,20 @@
+package Patterns;
+import java.util.Scanner;
+public class Reverse_Number_Triangle {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter the no. of times to run the loop : ");
+        int n = r.nextInt();
+        int x=1;
+        for(int i=0; i<n; i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=x;j<=n;j++){
+                System.out.print(j+" ");
+            }
+            x++;
+            System.out.println();
+        }
+    }
+}
