2ff7bdcecdf268378724e33f080e3ad0dfdcc9b3
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:19:05 +0530

    Update Hollow_Square.java

--- Hollow_Square.java
+++ Hollow_Square.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Hollow_Square {
     public static void main(String[] args){
         Scanner r = new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop : ");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n = r.nextInt();
         for(int i=0;i<n;i++){
             for(int j=0; j<n; j++){             //loop to print the pattern
