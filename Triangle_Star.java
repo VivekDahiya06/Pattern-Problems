1550a938e041059d3dff300b1e941ac51c5895ac
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:23:28 +0530

    Update Triangle_Star.java

--- Triangle_Star.java
+++ Triangle_Star.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Triangle_Star {
     public static void main(String[] args) {
         Scanner r = new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop :");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n = r.nextInt();
         for(int i=0; i<n ;i++){
             for(int j=n-1; j>i; j--){
