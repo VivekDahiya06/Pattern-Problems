c865d61e010d8c54caa7b1d6c735bb24c8e3202d
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:22:33 +0530

    Update Square_Fill.java

--- Square_Fill.java
+++ Square_Fill.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Square_Fill {
     public static void main(String[] args) {
         Scanner r=new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop : ");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n =r.nextInt();
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
