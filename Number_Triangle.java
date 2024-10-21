a15fac5f39a48052565dcfae5bf022ebab43be1e
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:20:54 +0530

    Update Number_Triangle.java

--- Number_Triangle.java
+++ Number_Triangle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Number_Triangle {
     public static void main(String[] args) {
         Scanner r=new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop : ");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n = r.nextInt();
         int x=1;
         for(int i=0; i<n; i++){        //Loop to print the pattern
