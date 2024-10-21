90b7134392fe2c2f3a1a38b84571bb8fc7786241
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:20:40 +0530

    Update Number_Increasing_Reverse_Pyramid.java

--- Number_Increasing_Reverse_Pyramid.java
+++ Number_Increasing_Reverse_Pyramid.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Number_Increasing_Reverse_Pyramid {
     public static void main(String[] args) {
         Scanner r=new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop : ");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n = r.nextInt();
         for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
