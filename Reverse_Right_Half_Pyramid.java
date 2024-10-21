03f6378c94d4d40250b0316c1a142cb675df1a65
Vivek Dahiya <146949211+VivekDahiya06@users.noreply.github.com>
2024-10-21 12:21:53 +0530

    Update Reverse_Right_Half_Pyramid.java

--- Reverse_Right_Half_Pyramid.java
+++ Reverse_Right_Half_Pyramid.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Reverse_Right_Half_Pyramid {
     public static void main(String[] args) {
         Scanner r=new Scanner(System.in);
-        System.out.println("Enter the no. of times to run the loop : ");
+        System.out.print("Enter the no. of times to run the loop : ");
         int n = r.nextInt();
         for(int i=n; i>0; i--){
             for(int j=0; j<i; j++){
